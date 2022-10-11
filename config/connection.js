94251c90a55f2dd886c5e8f8f0028bcb58a13620
Kenan <mckenzie.kenan@yahoo.com>
2022-10-11 17:56:55 -0400

    14.1

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 //This sets up the application to connect to the database using sequilize
 
 // import the sequilize constructor from the library
-const Sequelize  = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
 
 
-//create connection to our database, pass in your MySQL info for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-  
-  module.exports = sequelize;
-//This imports the base sequelize class and uses it to create a new connection to the db
-//The sequelize function accepts the database name, MySQL username and password as parameters, then we also pass configuration setting. 
-//Then we export the connection
+}
+
+module.exports = sequelize;
 
